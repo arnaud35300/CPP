5d7878d14d123e2dc0231a1312d5be6cddbd43d0
arguilla <arguilla@student.42.fr>
2022-02-05 12:03:59 +0100

    Module 03 ex00: done

---
+++ 03/ex00/includes/ClapTrap.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arguilla <arguilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/05 11:03:55 by arguilla          #+#    #+#             */
+/*   Updated: 2022/02/05 12:02:03 by arguilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+class ClapTrap {
+
+public:
+
+	ClapTrap(std::string name);
+	ClapTrap(ClapTrap const & src );
+	~ClapTrap( void );
+
+	ClapTrap & operator=( ClapTrap  const & rhs );
+
+	void	attack(const std::string & target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+
+	const std::string &	getName(void) const;
+	int	getHitPoints(void) const;
+	int	getEnergyPoints(void) const;
+	int	getAttackDamage(void) const;
+
+protected:
+
+
+private:
+	ClapTrap( void );
+	std::string	_name;
+	int	_hit_points;
+	int	_energy_points;
+	int	_attack_damage;
+
+};
+
+std::ostream &	operator<<(std::ostream & o, ClapTrap const & rhs);
+
+#endif /* CLAPTRAP_H */
---
+++ 03/ex00/srcs/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arguilla <arguilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 11:40:31 by arguilla          #+#    #+#             */
+/*   Updated: 2022/02/05 12:03:18 by arguilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.hpp"
+
+int	main(void)
+{
+	ClapTrap	claptrap("Jean");
+
+	std::cout << claptrap;
+	claptrap.attack("Ennemy");
+
+	claptrap.takeDamage(2);
+	std::cout << claptrap;
+	claptrap.beRepaired(1);
+	std::cout << claptrap;
+	
+	claptrap.takeDamage(20);
+	std::cout << claptrap;
+
+	claptrap.beRepaired(50);
+	claptrap.attack("Ennemy");
+	return (0);
+}
